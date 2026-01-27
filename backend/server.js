3b99e8b5cd6f605053d3d276d2d2754c6659df75
Sudarshan Patil <sudarshanpatil486@gmail.com>
2026-01-27 11:16:04 +0530

    Serverless function

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,6 @@ app.get('/', (req, res) => {
     res.send(`API Running`)
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log(`Server is running on port ${process.env.PORT}`);
+// });
